8b0c77597fb38364df64ff28a156b7081dddcaf7
tankayeva <ulzhan.tankayeva@gmail.com>
2019-08-13 11:26:37 +0600

    Create Main.java

---
+++ Problems/arrays/BattleshipsInABoard_419/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        char[][] board = {{'X', 'X', 'X', '.', 'X'}};
+        int result = s.countBattleships(board);
+        System.out.print(result);
+    }
+}
