9da70f6806709b561fea1fb88a6d09354abc822b
tankayeva <ulzhan.tankayeva@gmail.com>
2022-10-04 05:56:39 +0400

    Update Solution1217.java

--- Problems/arrays/Solution1217.java
+++ Problems/arrays/Solution1217.java
@@ -1,5 +1,15 @@
 package arrays;
 
+
+/*You might initially think we need to try all possibilities, possibly using dynamic programming. But the question is an "easy" for a reason.
+
+The key observation here is that it's free to move a chip from an even number to another even number, and it is free to move a chip from an odd number to another odd number. It only costs to move an even to an odd, or an odd to an even. Therefore, we want to minimise such moves.
+
+All chips must be on the same position once we're done, which is either even or odd. Therefore, we want to calculate whether it is cheaper to move all the odd chips to even or all the even chips to odd. This will simply come down to how many even chips we start with, and how many odd chips. Each chip that has to be moved will cost exactly 1.
+
+To determine the cost, we need to count how many are even, and how many are odd, and then take the minimum of these two values.
+*/
+
 class Solution1217 {
     public int minCostToMoveChips(int[] position) {
         int even = 0;
